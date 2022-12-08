319c5d39dbe6d79dbffba88a182fa1b5ce1efeea
wilberquito <wbq.software@gmail.com>
2022-12-08 20:13:18 +0100

    Uses data pipe chanel to intercomunicate between processess

--- learner/learner.py
+++ learner/learner.py
@@ -1,9 +1,10 @@
+import math
 import pandas as pd
 import sklearn.cluster as sklc 
 import from kmeans_util as ku
-import math
+import from export_util as eu
 
-training_csv = 'training.csv'
+training_csv = '../data/training.csv'
 
 X = pd.read_csv(training_csv)
 X_train, X_test = train_test_split(X, test_size = .15, random_state = 73)
@@ -24,5 +25,5 @@ for k, s, c in ss:
     if s >= s_optimal:
         k_optimal, s_optimal, centroids = s, k, c
 
-ku.export(k_optimal, centroids)
+eu.export(k_optimal, centroids, '../data/param.out')
 
