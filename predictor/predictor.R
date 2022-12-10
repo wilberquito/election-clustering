ca27a3fbb30693b0e382c88efe472043a289a7ac
wilberquito <wbq.software@gmail.com>
2022-12-10 14:12:32 +0100

    Closest centroid using euclidian distance

--- predictor/predictor.R
+++ predictor/predictor.R
@@ -1,7 +1,7 @@
 
-params.filename = '../data/param.out'
-testing.filename = '../data/testing.out'
-clustering.filename = '../clustering.out'
+params.filename <- '../data/param.out'
+testing.filename <- '../data/testing.csv'
+clustering.filename <- '../clustering.out'
 
 centroids <- read.table(params.filename, header=F, sep=",", dec=".", skip=1)
 n <- read.table(params.filename, header=F, sep=",", dec=".", nrows=1)[1,1]
@@ -13,9 +13,17 @@ if (n != nrow(centroids)) {
 euclidean <- function(a, b) sqrt(sum((a - b)^2))
 
 closest_centroid <- function (x, centroids) {
-  K = nrows(centroids)
+  K = nrow(centroids)
   i.min = sapply(1:K, function(i) {
-    euclidian(x, centroids[,i])
+    x <- euclidean(x, centroids[i,])
+    print(x)
+    x
   }) |> which.min()
+  print('---')
+  i.min
 }
 
+testing.df <- read.csv(testing.filename, header=F, sep=',')
+
+clusterization <- sapply(testing.df, closest_centroid, centroids=centroids)
+clusterization
