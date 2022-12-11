423622c72c24799bff3e7a09251b09e000cc1959
wilberquito <wbq.software@gmail.com>
2022-12-11 13:58:50 +0100

    Uses jupyter notation to see the execution in time

--- learner/learner.py
+++ learner/learner.py
@@ -29,17 +29,20 @@ ax.set(title='Elbow plot',
 #%%
 ss = []
 
-for n in range(1, 8):
+for k in clusters:
     try:
-        X_train, X_test = ku.train_test_split(n, X)
-        train_model = sklc.KMeans(n_clusters=n, random_state=73, n_init='auto').fit(X_train)
-        test_model = sklc.KMeans(n_clusters=n, random_state=73, n_init='auto').fit(X_test)
+        X_train, X_test = ku.train_test_split(k, X)
+        train_model = KMeans(n_clusters=k, random_state=73, n_init='auto').fit(X_train)
+        test_model = KMeans(n_clusters=k, random_state=73, n_init='auto').fit(X_test)
         train_centroids = train_model.cluster_centers_
-        s = ku.prediction_strength(n, train_centroids, X_test.to_numpy(), test_model.labels_)
-        ss.append((n, s, train_centroids))
+        ps = ku.prediction_strength(k, train_centroids, X_test.to_numpy(), test_model.labels_)
+        ss.append((k, ps, train_centroids))
     except Exception as e:
         print('Maybe too few samples...', e)
-        
+#%%
+pss = list(map(lambda s : s[1], ss))
+pss
+#%%        
 k_optimal, s_optimal, centroids = -math.inf, -math.inf, []
 
 for k, s, c in ss:
