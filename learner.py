59c73f9809ba875acd2f304fb70d13dda47a999b
wilberquito <wbq.software@gmail.com>
2022-12-14 00:45:13 +0100

    Standarization of the data compared to the number of poblation

--- learner.py
+++ learner.py
@@ -11,8 +11,12 @@ training_csv = './training.csv'
 X = pd.read_csv(training_csv, header=None)
 X = X.dropna(axis=0, how='any')
 
+pob = X[X.columns[-1]]
+X = X.drop(X.columns[-1], axis=1)
+X = X.div(pob, axis=0)
 #%%
-clusters = range(1, 8)
+K = 7
+clusters = range(1, K + 1)
 wss_list = []
 
 for k in clusters:
@@ -26,27 +30,14 @@ ax.plot(clusters, wss_list, '-o', color='black')
 ax.set(title='Elbow plot', 
        xlabel='number of clusters', 
        ylabel='WSS');
-#%%
-ss = []
 
-for k in clusters:
-    try:
-        X_train, X_valid = lc.train_test_split(k, X)
-        train_model = KMeans(n_clusters=k, random_state=73, n_init='auto').fit(X_train)
-        valid_model = KMeans(n_clusters=k, random_state=73, n_init='auto').fit(X_valid)
-        C_train, C_valid = train_model.predict(X_valid), valid_model.labels_
-        train_centroids = train_model.cluster_centers_
-        ps = lc.classic_prediction_strength(k, C_train, C_valid)
-        ss.append((k, ps, train_centroids))
-    except Exception as e:
-        print('Unexpected error...', e)
 #%%
-strengths = list(map(lambda s : s[1], ss))
-strengths
+results = lc.prediction_strength_of_clusters(X, K)
 # %%
-threshold = 0.85
+threshold = 0.8
+ps = list(map(lambda x : x[1], results))
 _, ax = plt.subplots()
-ax.plot(clusters, strengths, '-o', color='black')
+ax.plot(clusters, ps, '-o', color='black')
 ax.axhline(y=threshold, c='red');
 ax.set(title='Determining the optimal number of clusters', 
        xlabel='number of clusters', 
@@ -56,7 +47,7 @@ k_optimal = -math.inf
 s_optimal = -math.inf
 centroids = []
 
-for k, s, c in ss:
+for k, s, c in results:
     if s > threshold:
         k_optimal, s_optimal, centroids = k, s, c
 
